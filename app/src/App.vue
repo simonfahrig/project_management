184251a2cc97c68d1c5b8211d3b8d1d7c96df433
Simon Fahrig <simon.fahrig@yahoo.com>
2026-07-02 22:18:22 +0200

    initial commit

---
+++ app/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import VueMermaidString from 'vue-mermaid-string'
+import endent from 'endent'
+const text = ref("no node")
+const diagram = computed(() => 'graph BT\n  E(Ebene) --> L(Land)\nE --> A(AnderesLand)\nclick E\nclick L\nclick A')
+function nodeClick(nodeId){
+  text.value = nodeId;
+  console.log(nodeId)
+}
+</script>
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <vue-mermaid-string :value="diagram" @node-click="nodeClick"/>
+  <p>{{ text }}</p>
+</template>
+
+<style scoped></style>
